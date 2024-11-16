e1703efd2bbf96892d98dddf617a7cfc74b55808
claudia13-prog <cappellaclaudia13@gmail.com>
2024-11-16 15:41:11 +0100

    created guest list array

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -2,4 +2,12 @@ package com.ferragnez.party;
 
 public class CheckGuest {
 
+    // Inserisco dentro la classe il metodo main
+    public static void main(String[] args) {
+
+        // creare e inizializzare l’array contenente i nomi degli invitati
+        String[] guestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        
+    }
+
 }
