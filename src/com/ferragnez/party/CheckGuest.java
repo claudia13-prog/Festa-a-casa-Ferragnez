f9280c5434b53c40ede607bf51ce93a325ad52c2
claudia13-prog <cappellaclaudia13@gmail.com>
2024-11-16 16:53:12 +0100

    added for cycle and if condition

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -10,10 +10,31 @@ public class CheckGuest {
         // creare e inizializzare l’array contenente i nomi degli invitati
         String[] guestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
         
+        // chiedere all’utente come si chiama
         Scanner input = new Scanner(System.in);
         System.out.println("Come ti chiami?");
         String nameUser = input.nextLine();
-        System.out.println(nameUser);
+
+        // verificare che il nome sia presente nella lista
+        // lasciarlo entrare o rispedirlo cortesemente da dove è venuto
+        boolean isInvited = false;
+
+        for(int i = 0; i < guestList.length; i++){
+           
+            if(nameUser.equals(guestList[i])){
+                isInvited = true;
+            } 
+        };
+
+        if(isInvited){
+            System.out.println("Puoi entrare");
+
+        }else{
+            System.out.println("Torna da dove sei venuto");
+
+        };
+
+
 
 
         input.close();
