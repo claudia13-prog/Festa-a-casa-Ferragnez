6b8f133f9580847e03e68e6a1f4fe451ee6c4870
claudia13-prog <cappellaclaudia13@gmail.com>
2024-11-16 16:03:22 +0100

    added scanner input

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,7 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
     // Inserisco dentro la classe il metodo main
@@ -8,6 +10,14 @@ public class CheckGuest {
         // creare e inizializzare l’array contenente i nomi degli invitati
         String[] guestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
         
+        Scanner input = new Scanner(System.in);
+        System.out.println("Come ti chiami?");
+        String nameUser = input.nextLine();
+        System.out.println(nameUser);
+
+
+        input.close();
+
     }
 
 }
