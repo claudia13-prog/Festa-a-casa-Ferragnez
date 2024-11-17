839ad037e94b55aa88da0f406aec31981e020873
claudia13-prog <cappellaclaudia13@gmail.com>
2024-11-17 09:49:10 +0100

    strat new class bonus

---
+++ src/com/ferragnez/party/CheckGuestBonus.java
@@ -0,0 +1,46 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuestBonus {
+
+    // Inserisco dentro la classe il metodo main
+    public static void main(String[] args) {
+
+        // creare e inizializzare l’array contenente i nomi degli invitati
+        String[] guestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        
+        // chiedere all’utente come si chiama
+        Scanner input = new Scanner(System.in);
+        System.out.println("Come ti chiami?");
+        String nameUser = input.nextLine();
+
+        // verificare che il nome sia presente nella lista
+        boolean isInvited = false;
+
+        for(int i = 0; i < guestList.length; i++){
+           
+            if(nameUser.equals(guestList[i])){
+                isInvited = true;
+            } 
+        };
+        
+        
+        // lasciarlo entrare o rispedirlo cortesemente da dove è venuto
+
+        if(isInvited){
+            System.out.println("Puoi entrare");
+
+        }else{
+            System.out.println("Torna da dove sei venuto");
+
+        };
+
+
+
+
+        input.close();
+
+    }
+
+}
